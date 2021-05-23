0478f8e0549367ff5da6d877a48ccc45cf283117
Raymond Saade <raymond.saade@gmail.com>
2021-05-22 22:13:44 -0400

    added chapter page

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -1,8 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
-set :bind, '0.0.0.0'
+require "tilt/erubis"
+# set :bind, '0.0.0.0'
 set :port, 8080
 
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+  erb :chapter
 end
