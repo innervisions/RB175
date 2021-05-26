8b703309e216d21eb60102a27a9c83aacd7d9325
Raymond Saade <raymond.saade@gmail.com>
2021-05-26 16:02:45 -0400

    added search

--- 03_working_with_sinatra/book_viewer/book_viewer.rb
+++ 03_working_with_sinatra/book_viewer/book_viewer.rb
@@ -1,18 +1,61 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
-# set :bind, '0.0.0.0'
+set :bind, '0.0.0.0'
 set :port, 8080
 
-get "/" do
-  @title = "The Adventures of Sherlock Holmes"
+before do
   @contents = File.readlines("data/toc.txt")
+end
+
+get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  redirect "/" unless (1..@contents.size).cover?(number)
+  @title = "#{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
   erb :chapter
+end
+
+def each_chapter
+  @contents.each_with_index do |name, idx|
+    number = idx + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results if !query || query.empty?
+
+  each_chapter do |number, name, contents|
+    results << { number: number, name: name } if contents.include?(query)
+  end
+
+  results
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+not_found do
+  redirect "/"
+end
+
+helpers do
+  def in_paragraphs(text)
+    id = 0
+    text.split("\n\n").map do |paragraph|
+      id += 1
+      "<p id=#{id}>#{paragraph}</p>"
+    end.join
+  end
 end
